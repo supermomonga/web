9b9d6c1708e5ff6278f3d0c569864a2ee479f5a1
ne_Sachirou <utakata.c4se@gmail.com>
2014-02-07 20:39:12 +0900

    Add Google web master code.

--- templates/layout.php
+++ templates/layout.php
@@ -11,6 +11,7 @@ $home = "http://ranyuen.com$base";
 <head>
     <meta charset="UTF-8"/>
     <title><?php $h->h($title); ?> - Ranyuen</title>
+    <meta name="google-site-verification" content="osPpSihI4cWmpC3IfcU0MFq6zDdSPWyb7V2_ECHHo5Q"/>
     <link rel="home" href="<?php $h->h($home); ?>"/>
     <link rel="stylesheet" href="/assets/bower_components/normalize-css/normalize.css"/>
     <link href="http://fonts.googleapis.com/css?family=Alef:400,700" rel="stylesheet" type="text/css"/>
@@ -50,7 +51,7 @@ $home = "http://ranyuen.com$base";
                 <div class="news">
                     <img src="/assets/images/icons/feed.png"
                          width="30" height="30"/>NEWS
-                <?php $h->echoNav($news_nav); ?>
+                <?php $h->echoNav($news_nav, "$base/news/"); ?>
                 </div>
                 <a href="https://www.facebook.com/ranyuenjapan"
                    target="_blank"
@@ -84,22 +85,14 @@ $home = "http://ranyuen.com$base";
         </div>
     </div>
     <script src="/assets/javascripts/messageForDeveloperFromRanyuen.js"></script>
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-21495834-1']);
-        _gaq.push(['_setDomainName', 'ranyuen.com']);
-        _gaq.push(['_setAllowLinker', true]);
-        _gaq.push(['_trackPageview']);
-        (function () {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' == document.location.protocol ?
-                'https://ssl' : 'http://www') +
-                '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-47871400-1', 'ranyuen.com');
+        ga('send', 'pageview');
     </script>
 </body>
 </html>
